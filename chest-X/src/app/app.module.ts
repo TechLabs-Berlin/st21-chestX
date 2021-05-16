39c953e09220d36fca1f6aaba084f7284ed53621
bolajikoyi <omobolaji.koyi@yahoo.com>
2021-05-16 12:42:02 +0200

    Additional components created

--- chest-X/src/app/app.module.ts
+++ chest-X/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
